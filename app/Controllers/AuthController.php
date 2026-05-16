d7ffb5a042da0c6e6773a6f76e5fe3afa09fa1e3
sszawwr <nisalwa0845@gmail.com>
2026-05-16 10:47:56 +0700

    Capstone 6 Migration & Seeding

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -8,45 +8,59 @@ use CodeIgniter\HTTP\ResponseInterface;
 class AuthController extends BaseController
 {
     function __construct()
-{
-    helper('form');
-}
+    {
+        helper('form');
+    }
 
-public function login()
-{
-    if ($this->request->getPost()) {
-        $username = $this->request->getVar('username');
-        $password = $this->request->getVar('password');
+    public function login()
+    {
+        if ($this->request->getPost()) {
 
-        $dataUser = ['username' => 'Annisa Salwa', 'password' => '202cb962ac59075b964b07152d234b70', 'role' => 'admin']; // passw 123
+            $username = $this->request->getVar('username');
+            $password = $this->request->getVar('password');
 
-        if ($username == $dataUser['username']) {
-            if (md5($password) == $dataUser['password']) {
-                session()->set([
-                    'username' => $dataUser['username'],
-                    'role' => $dataUser['role'],
-                    'email' => '111202415962@mhs.dinus.ac.id',
-                    'login_time' => date('Y-m-d H:i:s'),
-                    'isLoggedIn' => TRUE
-                ]);
+            $dataUser = [
+                'username' => 'Annisa Salwa',
+                'password' => password_hash('1234567', PASSWORD_DEFAULT),
+                'role' => 'admin'
+            ];
 
-                return redirect()->to('/profile');
+            if ($username == $dataUser['username']) {
+
+                // cek password
+                if (password_verify($password, $dataUser['password'])) {
+
+                    session()->set([
+                        'username' => $dataUser['username'],
+                        'role' => $dataUser['role'],
+                        'email' => '111202415962@mhs.dinus.ac.id',
+                        'login_time' => date('Y-m-d H:i:s'),
+                        'isLoggedIn' => TRUE
+                    ]);
+
+                    return redirect()->to('/profile');
+
+                } else {
+
+                    session()->setFlashdata('failed', 'Username & Password Salah');
+                    return redirect()->back();
+                }
+
             } else {
-                session()->setFlashdata('failed', 'Username & Password Salah');
+
+                session()->setFlashdata('failed', 'Username Tidak Ditemukan');
                 return redirect()->back();
             }
-        } else {
-            session()->setFlashdata('failed', 'Username Tidak Ditemukan');
-            return redirect()->back();
-        }
-    } else {
-        return view('login');
-    }
-}
 
-public function logout()
-{
-    session()->destroy();
-    return redirect()->to('login');
-}
+        } else {
+
+            return view('login');
+        }
+    }
+
+    public function logout()
+    {
+        session()->destroy();
+        return redirect()->to('login');
+    }
 }
